1e7ddcf77fc19924206255acac52b7fa0b088527
wolftender <etheryt@protonmail.com>
2022-09-10 15:17:47 +0200

    express initialization

--- src/services/web.ts
+++ src/services/web.ts
@@ -5,11 +5,32 @@
 
 import { Logging } from "../logger";
 import { Service } from "../service";
+import config from '../config.json';
+
+import path from 'path';
+import express, { Express } from 'express';
 
 @Service ()
 export class WebService extends Logging {
+    private m_server : Express;
+
     public constructor () {
         super ();
         this.info ("initializing web service");
+
+        // initialize the web server
+        this.m_server = express ();
+
+        // read config variables
+        const port = config.web.port;
+        const staticPath = path.resolve (config.web.static);
+
+        // serve static files
+        this.m_server.use (express.static (staticPath));
+
+        this.m_server.listen (port, () => {
+            this.info (`server listening on port ${port}`);
+            this.info (`server will use static directory: ${staticPath}`);
+        });
     }
 }
